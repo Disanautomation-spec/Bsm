b0f4d176a9ce2de1313f545f4bf1d8b8e8bf060f
Disanautomation-spec <mwesigwaamon519@gmail.com>
2026-09-04 17:36:34 +0300

    Add files via upload

---
+++ install_and_run.py
@@ -0,0 +1,4 @@
+
+import subprocess, sys
+subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+subprocess.check_call([sys.executable, "-m", "streamlit", "run", "app.py"])
